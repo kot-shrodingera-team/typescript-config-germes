34f0bf3ec2ca0eb2b210914be1f3966c7a768df5
kot-shrodingera <kot.shrodingera@gmail.com>
2020-09-30 23:27:52 +0600

    refactor/chore: убраны зависимости от конфигов, обновлены зависимости

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,56 @@
 module.exports = {
-  extends: '@kot-shrodingera-team/eslint-config-germes-base/.eslintrc'
+  env: {
+    browser: true,
+  },
+  globals: {
+    worker: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  ignorePatterns: ['!.*', 'node_modules'],
+  overrides: [
+    {
+      files: ['*.js'],
+      extends: ['airbnb-base', 'plugin:prettier/recommended'],
+    },
+    {
+      files: ['.prettierrc.js'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          { devDependencies: true },
+        ],
+      },
+    },
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      settings: {
+        'import/resolver': {
+          typescript: {},
+        },
+      },
+    },
+    {
+      files: 'src/**/*',
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          {
+            devDependencies: true,
+          },
+        ],
+      },
+    },
+  ],
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,5 @@
-const germesConfig = require('@kot-shrodingera-team/prettier-config-germes/.prettierrc');
-
 module.exports = {
-  ...germesConfig,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  endOfLine: 'auto',
 };
